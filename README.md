805233568d84f2374199c75d859d9e953b185fc3
VictoSN <victorionanda1105@gmail.com>
2026-04-04 01:13:53 +0800

    added background logic and readme

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # weather-app
-Simple weather app made using HTML-CSS-JavaScript
+## Weather App using OpenWeather & Pexels API
+Weather App made using basic HTML-CSS-Javascript, utilizing the free tier API's of OpenWeather for geo-weather data and Pexels for background image.
+
+1. Search the weather from OpenWeather API
+2. Dynamically changing background using Pexels API
+3. Multiple unit of measurement
+
+## How it works
+- User input is processed to find the latitude and longitude of the city using OpenWeather's Geocoding API
+- The extracted latitude and longitude is used to find the city's data using OpenWeather's '5 Day forecast' data
+- The user input is also used to find a suitable background image using Pexel's API
+
+## Screenshots
+### Default Interface
+![Default Interface](images/Default.png)
+
+### Imperial Interface
+![Imperial Interface](images/Imperial.png)
+
+### Kelvin Interface
+![Kelvin Interface](images/Kelvin.png)
+
+### Empty Interface
+![Empty Tasks](images/Empty.png)
+
+### Error Interface
+![Error Interface](images/Error.png)
+
+## How to run
+1. Download the project
+2. Replace the placeholder with real API keys (OpenWeather's API must use the '5 day weather forecast' type)
+3. run index.html
--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const searchBox = document.getElementById("searchBox");
 const submitButton = document.getElementById("submitButton");
 const cityDiv = document.getElementById("cityDiv");
 
+function checkCity(cityData) {
+    console.log(cityData);
+    if(cityData.length !== 0) {
+        return true;
+    }
+        
+    return false;
+}
+
 async function fetchWeather() {
     const unitMeasurement = document.querySelector("input[name=unitRadio]:checked");
     const citySearched = searchBox.value;
@@ -14,32 +23,46 @@ async function fetchWeather() {
     const cityRes = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${citySearched}&limit=1&appid=${WEATHER_API_KEY}`)
     const cityData = await cityRes.json();
     
-    // Use the city data to fetch the weather data
-    const weatherRes = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${cityData[0].lat}&lon=${cityData[0].lon}&units=${units}&appid=${WEATHER_API_KEY}`)
-    const weatherData = await weatherRes.json();
+    if (checkCity(cityData)) {
+        // Use the city data to fetch the weather data
+        const weatherRes = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${cityData[0].lat}&lon=${cityData[0].lon}&units=${units}&lang=en&appid=${WEATHER_API_KEY}`)
+        const weatherData = await weatherRes.json();
+    
+        console.log(weatherData);
+        return weatherData;
+    } else {
+        cityDiv.insertAdjacentHTML("beforeend", `<div id="errorData">Please Enter a Valid City</div>`);
+    }
+}
 
-    console.log(weatherData);
-    return weatherData;
+function temperatureUnit() {
+    const unitMeasurement = document.querySelector("input[name=unitRadio]:checked");
+    let units = unitMeasurement.value;
+
+    switch (units) {
+        case "metric": return "C";
+        case "imperial": return "F";
+        case "kelvin": return "K";
+    }
 }
 
 function renderWeather(weatherData) {
     const name = weatherData.city.name;
-    const lat = weatherData.city.coord.lat;
-    const lon = weatherData.city.coord.lon;
 
-    const temp = weatherData.list[0].main.temp;
+    const temp = Math.round(weatherData.list[0].main.temp);
     const humidity = weatherData.list[0].main.humidity;
     const pressure = weatherData.list[0].main.pressure;
     const weather = weatherData.list[0].weather[0].main;
 
     return `
-        <div>
-            <h1 id="cityName">${name}</h1>
-            <text id="latLot">Lat: ${lat} | Lot: ${lon}</text>
-            <h3 id="temp">Temp: ${temp}</h3>
-            <h3 id="humid">Humidity: ${humidity}%</h3>
-            <h3 id="press">Pressure: ${pressure} hPa</h3>
-            <h3 id="weat">Weather: ${weather}</h3>
+        <div id="mainData">
+            <p id="cityName">${name}</p>
+            <p id="temp">${temp}°${temperatureUnit()}</p>
+            <p id="weat">${weather}</p>
+        </div>
+        <div id="additionalData">
+            <text id="humid">H: ${humidity}%</text>
+            <text id="humid">P: ${pressure} hPa</text>
         </div>
     `
 }
@@ -48,7 +71,7 @@ async function updateBackground() {
     const citySearched = searchBox.value;
 
     // Search for city's images using pexels
-    const url = `https://api.pexels.com/v1/search?query=${citySearched}&orientation=landscape&size=large&per_page=15`;
+    const url = `https://api.pexels.com/v1/search?query=${citySearched}&orientation=landscape&size=large&per_page=50`;
     const cityRes = await fetch(url, {
         headers: {
             Authorization: PHOTO_API_KEY
@@ -56,7 +79,7 @@ async function updateBackground() {
     });
     const cityPhoto = await cityRes.json();
 
-    const randomIndex = Math.floor(Math.random() * 15);
+    const randomIndex = Math.floor(Math.random() * 50);
     console.log(cityPhoto.photos[randomIndex].src);
 
     // Set image of city as background
@@ -81,7 +104,7 @@ async function processWeather() {
         cityDiv.insertAdjacentHTML("beforeend", renderWeather(await fetchWeather()));
         updateBackground();
     } else {
-        cityDiv.insertAdjacentHTML("beforeend", `<h3>Please Enter a Valid Input</h3>`);
+        cityDiv.insertAdjacentHTML("beforeend", `<div id="errorData">Please Enter a Valid Input</div>`);
     }
 }
 
@@ -101,28 +124,13 @@ document.addEventListener("keydown", (event) => {
 });
 
 window.onload = function() {
-    const randomIndex = Math.floor(Math.random() * 20);
+    const randomIndex = Math.floor(Math.random() * 50);
     const defaultCities = [
-        "London",
-        "New York",
-        "Tokyo",
-        "Paris",
-        "Dubai",
-        "Singapore",
-        "Hong Kong",
-        "Los Angeles",
-        "Chicago",
-        "Sydney",
-        "Toronto",
-        "Berlin",
-        "Rome",
-        "Barcelona",
-        "Amsterdam",
-        "Istanbul",
-        "Seoul",
-        "Bangkok",
-        "Moscow",
-        "Rio de Janeiro"
+        "London","Paris","New York","Tokyo","Dubai","Singapore","Hong Kong","Los Angeles","Chicago","San Francisco",
+        "Sydney","Melbourne","Toronto","Vancouver","Berlin","Rome","Madrid","Barcelona","Amsterdam","Vienna",
+        "Prague","Lisbon","Athens","Istanbul","Seoul","Beijing","Shanghai","Bangkok","Kuala Lumpur","Jakarta",
+        "Manila","Mumbai","Delhi","Bangalore","Cairo","Cape Town","Johannesburg","Moscow","Saint Petersburg","Zurich",
+        "Geneva","Stockholm","Oslo","Copenhagen","Helsinki","Dublin","Edinburgh","Manchester","Birmingham","Boston"
     ];
 
     searchBox.value = defaultCities[randomIndex];
